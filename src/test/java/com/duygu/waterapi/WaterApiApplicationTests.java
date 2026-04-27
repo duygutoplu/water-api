2991bf4d7e90ede0d40bfc04ed9d9e0ad9e50890
duygutopluu <toplud5118679_aehit@students.vizja.pl>
2026-04-28 00:10:21 +0200

    Initial project setup and add WaterIntake model

---
+++ src/test/java/com/duygu/waterapi/WaterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duygu.waterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
